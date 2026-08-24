0bcfc28db7cbc20ea9c5a7c5e4530e813c0218f9
student <hibiki0205i@icloud.com>
2026-08-24 10:46:56 +0900

    練習

---
+++ 20260824/202060824_exp.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	//”z—ñ
+	int ary[5];
+	int* pAry;
+	//pAry‚Í”z—ñ‚Ìæ“ª‚ğw‚·
+	pAry = ary;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "&ary[" << i << "];" << &ary[i] << endl;
+		cout << "pAry:" << pAry + i << endl;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> *(pAry + i);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "pAry:" << *(pAry + i) << endl;
+	}
+	return 
+}
