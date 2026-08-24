4528987ec8983ed3c92d4b283b86a69ac697e7c1
student <hibiki0205i@icloud.com>
2026-08-24 11:34:12 +0900

    演習３

---
+++ 20260824/20260824_prac2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int numbers[5] = { 35,82,17,96,54 };
+	int* pNum;
+	int max;
+
+	pNum = numbers;
+	for (int i = 0; i < 5; i++)
+	{
+	
+		if (i == 0)
+		{
+			max = *(pNum + i);
+		}
+		if (max < *(pNum + i))
+		{
+			max = *(pNum + i);
+		}
+	}
+	cout << "Å‘å’l" << max << endl;
+	return 0;
+}
